e96db35be5bb4e1b5e2dcc4f3213928bc5ca4300
mojahige <mojahige@gmail.com>
2020-04-17 10:22:58 +0900

    cosme

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,23 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": ["error", { singleQuote: true, semi: true }],
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
   },
 };
